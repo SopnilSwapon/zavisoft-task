9d5cf87590d38ce583907bf69962a146930d890f
SopnilSwapon <sopnilswapon1@gmail.com>
2026-02-23 04:59:47 +0600

    Delete global loading state; refactor loading indicators

--- src/app/loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loading() {
-  // TODO: Will replace with a better loading state, maybe a spinner or skeletons
-  return (
-    <div className="min-h-screen p-6">
-      <h1 className="text-center">Loading...</h1>
-    </div>
-  );
-}
